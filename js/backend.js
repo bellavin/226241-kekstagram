a4276d2a747a661bd1f29bd11dbe736c8cb8d269
Dima Bellavin <izume@inbox.ru>
2018-06-30 09:44:20 +0300

    Бэкэнд

--- js/backend.js
+++ js/backend.js
@@ -4,7 +4,6 @@
   var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
   var URL_UPLOAD = 'https://js.dump.academy/kekstagram';
 
-
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
